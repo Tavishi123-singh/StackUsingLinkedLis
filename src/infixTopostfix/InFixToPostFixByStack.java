80c7bb2e7d81434b063ecef52854cad6d265a261
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-12 01:00:48 +0530

    Final Commit

--- src/infixTopostfix/InFixToPostFixByStack.java
+++ src/infixTopostfix/InFixToPostFixByStack.java
@@ -1,4 +1,21 @@
 package infixTopostfix;
 
+import stack.MyStack;
+
+import java.util.Scanner;
+
 public class InFixToPostFixByStack {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the expression: ");
+        String expression=sc.nextLine();
+    }
+    public String convertToPostFix(String expression){
+        String res="";
+        MyStack obj=new MyStack();
+        for (int i = 0; i <expression.length() ; i++) {
+            char c=expression.charAt(i);
+        }
+        return res;
+    }
 }
