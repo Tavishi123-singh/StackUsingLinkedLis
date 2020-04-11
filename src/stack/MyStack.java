4962901d5a9447b9e2f1c03dee2f96b65dc0e017
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-12 00:53:04 +0530

    Final Commit

--- src/stack/MyStack.java
+++ src/stack/MyStack.java
@@ -1,4 +1,46 @@
 package stack;
 
 public class MyStack {
+    Node head;
+    int size;
+    public MyStack(){
+        head=null;
+        size=0;
+    }
+    public void push(int element){
+        Node node=new Node(element);
+        node.setNext(head);
+        head=node;
+        size++;
+    }
+    public Node pop(){
+        Node res=null;
+        if (!isEmpty()){
+            res=head;
+            size--;
+            head=head.getNext();
+        }
+        else {
+            System.out.println("Stack is Underflow");
+        }
+        return res;
+    }
+
+    public boolean isEmpty() {
+        boolean res=false;
+        if (head==null){
+            res=true;
+        }
+        return res;
+    }
+    public Node peek(){
+        Node res=null;
+        if(!isEmpty()){
+            res=head;
+        }
+        else {
+            System.out.println("Stack is Underflow");
+        }
+        return res;
+    }
 }
