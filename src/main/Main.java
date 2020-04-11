3f3a49bceeba80d29241d1666cb3ec0e58a043f3
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-12 01:04:45 +0530

    Final Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,12 @@
 package main;
 
+import stack.MyStack;
+
 public class Main {
+    public static void main(String[] args) {
+        MyStack obj=new MyStack();
+        obj.peek();
+        obj.pop();
+        System.out.println("Size of Stack: "+obj.);
+    }
 }
--- src/stack/MyStack.java
+++ src/stack/MyStack.java
@@ -43,4 +43,7 @@ public class MyStack {
         }
         return res;
     }
+    public int size(){
+        return size;
+    }
 }
